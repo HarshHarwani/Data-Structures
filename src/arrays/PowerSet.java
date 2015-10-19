b7bc7e99d7f32f4ad74f8d845674a3aafb98eef5
HarshHarwani <harwaniharsh@gmail.com>
2015-10-18 22:15:23 -0400

    Modified generate powerSet problem

--- src/arrays/PowerSet.java
+++ src/arrays/PowerSet.java
@@ -5,35 +5,33 @@ public class PowerSet {
 
     
     
-    public static Set<Set<Integer>> powerSet(Set<Integer> originalSet) {
-        Set<Set<Integer>> sets = new HashSet<Set<Integer>>();
-        if (originalSet.isEmpty()) {
+    public static Set<Set<Integer>> getPowerSet(Set<Integer> originalSet){
+        Set<Set<Integer>> sets=new HashSet<Set<Integer>>();
+        if(originalSet.isEmpty()){
             sets.add(new HashSet<Integer>());
             return sets;
         }
-        List<Integer> list = new ArrayList<Integer>(originalSet);
-        Integer head = list.get(0);
-        Set<Integer> rest = new HashSet<Integer>(list.subList(1, list.size()));
-        Set<Set<Integer>> interResult=powerSet(rest);
-        for (Set<Integer> set : interResult) {
-            Set<Integer> newSet = new HashSet<Integer>();
+        List<Integer> list=new ArrayList<Integer>(originalSet);
+        Integer head=list.get(0);
+        Set<Integer> rest=new HashSet<Integer>(list.subList(1,list.size()));
+        Set<Set<Integer>> interResult=getPowerSet(rest);
+        for(Set<Integer> ele: interResult){
+            Set<Integer> newSet=new HashSet<Integer>();
             newSet.add(head);
-            newSet.addAll(set);
+            newSet.addAll(ele);
             sets.add(newSet);
-            sets.add(set);
+            sets.add(ele);
         }
         return sets;
     }
     
-    
-    
-    
+
     public static void main(String args[]){
         Set<Integer> set=new HashSet<Integer>();
         set.add(1);
-        set.add(2);
+        set.add(2); 
         set.add(3);
-        Set<Set<Integer>> resultSet=powerSet(set);
+        Set<Set<Integer>> resultSet=getPowerSet(set);
         for(Set<Integer> ele:resultSet){
             System.out.println(ele.toString());
         }
